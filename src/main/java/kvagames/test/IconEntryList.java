6e91d7c56887f7e67e105c569cde85ff829090f0
KvaGram <0kvakk@gmailcom>
2019-12-04 19:36:44 +0100

    optimize IconEntryList
    The need to cunstruct the regex pattern every time a text needs to be split seems wastefull. Better to save it, and raise a flag for when it needs an update.

--- src/main/java/kvagames/test/IconEntryList.java
+++ src/main/java/kvagames/test/IconEntryList.java
@@ -4,12 +4,13 @@ import org.apache.commons.lang3.ArrayUtils;
 import java.awt.*;
 
 /**
- * An entry for an icon-lookuptext binding.
- * Meant so an icon can be rendered in place of some text.
+ * A lookup list binding lookup keywords to Image icons.
  */
 public class IconEntryList {
     private String[] lookups;
     private Image[] icons;
+    private boolean needUpdateFlag;
+    private String searchPattern;
 
     public IconEntryList() {
         this(new String[0], new Image[0]);
@@ -18,12 +19,35 @@ public class IconEntryList {
     public IconEntryList(String[] lookupNames, Image[] icons) {
         this.lookups = lookupNames;
         this.icons = icons;
+        needUpdateFlag = true;
     }
 
     public void Add(String lookup, Image icon){
         ArrayUtils.add(lookups, lookup.toUpperCase());
         ArrayUtils.add(icons, icon);
+        needUpdateFlag = true;
     }
+    public void Add(String[] lookups, Image[] icons){
+        if(lookups.length != icons.length)
+            throw new IllegalArgumentException("Length of lookups to add does not match length of icons");
+        for (int i = 0; i < lookups.length; i++) {
+            Add(lookups[i], icons[i]);
+        }
+    }
+    public void Remove(String lookup) {
+        int index = ArrayUtils.indexOf(lookups, lookup);
+        if (index < 0)
+            return;
+        ArrayUtils.remove(lookups, index);
+        ArrayUtils.remove(icons, index);
+        needUpdateFlag = true;
+    }
+    public void Remove(String[] lookups){
+        for (String lookup : lookups) {
+            Remove(lookup);
+        }
+    }
+
 
     public String[] getLookups() {
         return lookups;
@@ -39,11 +63,11 @@ public class IconEntryList {
         return getScaledIcon(ArrayUtils.indexOf(lookups, lookup), size);
     }
     public String[] SplitText(String text) {
-        StringBuffer pattern = new StringBuffer();
-        pattern.append(String.join("|", lookups));
-        pattern.insert(0, "[");
-        pattern.append("]");
-        String[] ret = text.split(pattern.toString());
+        if(needUpdateFlag) {
+            searchPattern = "[" + String.join("|", lookups) + "]";
+            needUpdateFlag = false;
+        }
+        String[] ret = text.split(searchPattern);
         return ret;
     }
 }
